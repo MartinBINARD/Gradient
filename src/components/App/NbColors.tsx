2a08a60c73d853de9b2ba196db9e30ab66ada464
MartinBINARD <martin.binard.dev@gmail.com>
2023-08-31 11:22:13 +0200

    React Redux : lecture du state → useSelector

--- src/components/App/NbColors.tsx
+++ src/components/App/NbColors.tsx
@@ -1,5 +1,33 @@
+import { useSelector } from 'react-redux';
+
 function NbColors() {
-  return <div className="nbColors">XXX couleur(s) générée(s)</div>;
+  /*
+    Pour LIRE les données depuis le store,
+    on utilise `useSelector`.
+
+    `useSelector` prend un callback qui a le state en paramètre
+    et retourne notre valeur.
+
+    Ce callback est un **selector** (d'où le nom du hook)…
+    C'est-à-dire une fonction qui, à partir du state, retourne
+    tout ou partie du state, calculée ou non
+
+    - « tout ou partie » : retourner le state en entier ou seulement une portion
+    - « calculée ou non » : retourne la valeur telle quelle ou après un calcul
+        (traitement)
+
+    Rappel : quand on a plusieurs reducer, on n'oublie qu'il est
+    ajouté au state (global) en propriété
+    → state.color.nbColors
+
+    En parallèle, `useSelector` abonne/souscrit (_subscribe_) le composant
+    à la modification de ce qui est retourné par le _selector_
+    → ici, à chaque fois que `nbColors`,
+      le composant `<NbColors />` sera re-rendu
+  */
+  const nbColors = useSelector((state) => state.color.nbColors);
+
+  return <div className="nbColors">{nbColors} couleur(s) générée(s)</div>;
 }
 
 export default NbColors;
