ea0ad1f54c6e75b16c4eac2fef996621ed540f6f
MartinBINARD <martin.binard.dev@gmail.com>
2023-08-31 13:17:51 +0200

    React Redux : Dispatch dans ColorsButton

--- src/components/App/ColorButtons.tsx
+++ src/components/App/ColorButtons.tsx
@@ -1,13 +1,38 @@
+import { useDispatch } from 'react-redux';
+
+import { changeFirstColor, changeLastColor } from '../../store/reducers/color';
+import { randomHexColor } from '../../utils/color';
+
 function ColorButtons() {
+  // https://react.dev/warnings/invalid-hook-call-warning
+  const dispatch = useDispatch();
+
+  const handleClickFirst = () => {
+    // J'émets mon intention
+    // je dispatch une action
+    dispatch(changeFirstColor(randomHexColor()));
+  };
+
+  const handleClickLast = () => {
+    // J'émets mon intention
+    // je dispatch une action
+    dispatch(changeLastColor(randomHexColor()));
+  };
+
+  const handleClickAll = () => {
+    handleClickFirst();
+    handleClickLast();
+  };
+
   return (
     <div className="buttons group">
-      <button type="button" className="button">
+      <button type="button" className="button" onClick={handleClickFirst}>
         Random First
       </button>
-      <button type="button" className="button">
+      <button type="button" className="button" onClick={handleClickAll}>
         Random All
       </button>
-      <button type="button" className="button">
+      <button type="button" className="button" onClick={handleClickLast}>
         Random Last
       </button>
     </div>
